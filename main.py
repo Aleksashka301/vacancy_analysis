97261dd9c7d13aa0068aaa11e4d829f3d942d4df
Aleksashka301 <aleksashka301@gmail.com>
2025-02-21 18:49:03 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
 import requests
 from environs import Env
 from terminaltables import AsciiTable
+from pprint import pprint
 
 
-def request_response(url, language, location=None, headers=None):
+def request_response(url, params, headers=None):
     headers = headers or {}
-    params = {}
-
-    if type(location) == int:
-        params['per_page'] = 20
-        params['text'] = language
-        params['area'] = location
-    elif type(location) == str:
-        params['keywords'] = language
-        params['town'] = location
-
     response = requests.get(url, headers=headers, params=params, timeout=5)
     response.raise_for_status()
-
+    pprint(response.json())
     return response.json()
 
 
@@ -46,58 +37,75 @@ def predict_rub_salary_sj(vacancy):
         return vacancy['payment_to'] + 0.8
 
 
-def predict_salary(vacancies, resource):
-    salarys, count = 0, 0
+def predict_salary(vacancies, function):
+    sum_income, count = 0, 0
     for vacancy in vacancies:
-        if resource == 'hh':
-            salary_info = predict_rub_salary_hh(vacancy)
-        elif resource == 'sj':
-            salary_info = predict_rub_salary_sj(vacancy)
-
-        if salary_info:
-            salarys += salary_info
+        expected_income = function(vacancy)
+        if expected_income:
+            sum_income += expected_income
             count += 1
-
-    return count, int(salarys / count)
+    try:
+        return count, int(sum_income / count)
+    except ZeroDivisionError:
+        return 0, 0
 
 
 if __name__ == '__main__':
     env = Env()
     env.read_env()
     sj_secret_key = env.str('SUPERJOB_KEY')
+    hh_town = 1
+    sj_town = 'Москва'
+    num_vacancies = 100
 
-    url_hh = 'https://api.hh.ru/vacancies'
-    url_superjob = 'https://api.superjob.ru/2.0/vacancies/'
-    headers_sj = {
+    hh_url = 'https://api.hh.ru/vacancies'
+    sj_url = 'https://api.superjob.ru/2.0/vacancies/'
+    sj_headers = {
         'X-Api-App-Id': sj_secret_key,
+    }
+
+    hh_params = {
+        'per_page': num_vacancies,
+        'text': None,
+        'area': hh_town,
+    }
+    sj_params = {
+        'keywords': None,
+        'town': sj_town,
+        'count': num_vacancies,
     }
 
     programming_languages = [
         'JavaScript',
-        'Java',
-        'Python',
-        '1С программист',
-        'PHP',
-        'C++',
-        'C#',
-        'C программист',
+        # 'Java',
+        # 'Python',
+        # '1С программист',
+        # 'PHP',
+        # 'C++',
+        # 'C#',
+        # 'C программист',
     ]
-    vacancies_statistics_hh = [['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']]
-    vacancies_statistics_sj = [['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']]
+    hh_vacancies_statistics = [['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']]
+    sj_vacancies_statistics = [['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']]
 
     for language in programming_languages:
-        vacancies_hh = request_response(url_hh, language, 1)
-        vacansies_sj = request_response(url_superjob, language, 'Москва', headers_sj)
-        average_salary_hh = predict_salary(vacancies_hh['items'], 'hh')
-        average_salary_sj = predict_salary(vacansies_sj['objects'], 'sj')
+        hh_params['text'] = language
+        sj_params['keywords'] = language
+        hh_vacancies = request_response(hh_url, hh_params)
+        sj_vacansies = request_response(sj_url, sj_params, sj_headers)
 
-        vacancies_statistics_hh.append([language, vacancies_hh['found'], average_salary_hh[0], average_salary_hh[1]])
-        vacancies_statistics_sj.append([language, vacansies_sj['total'], average_salary_sj[0], average_salary_sj[1]])
+        hh_num_vacancies = predict_salary(hh_vacancies['items'], predict_rub_salary_hh)[0]
+        hh_average_salary = predict_salary(hh_vacancies['items'], predict_rub_salary_hh)[1]
+        sj_num_vacancies = predict_salary(sj_vacansies['objects'], predict_rub_salary_sj)[0]
+        sj_average_salary = predict_salary(sj_vacansies['objects'], predict_rub_salary_sj)[1]
 
-    table_hh = AsciiTable(vacancies_statistics_hh)
-    table_sj = AsciiTable(vacancies_statistics_sj)
-    table_hh.title = 'HeadHunter Moscow'
-    table_sj.title = 'SuperJob Moscow'
+        hh_vacancies_statistics.append([language, hh_vacancies['found'], hh_num_vacancies, hh_average_salary])
+        sj_vacancies_statistics.append([language, sj_vacansies['total'], sj_num_vacancies, sj_average_salary])
 
-    print(table_hh.table)
-    print(table_sj.table)
+    hh_table = AsciiTable(hh_vacancies_statistics)
+    sj_table = AsciiTable(sj_vacancies_statistics)
+    hh_table.title = 'HeadHunter Moscow'
+    sj_table.title = 'SuperJob Moscow'
+
+    print(hh_table.table)
+    print(sj_table.table)
