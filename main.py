30c96d76aa88785e13c71f258db3fb4f4d59808a
Aleksashka301 <aleksashka301@gmail.com>
2025-02-22 19:23:26 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import requests
 from environs import Env
 from terminaltables import AsciiTable
-from pprint import pprint
 
 
 def request_response(url, params, headers=None):
     headers = headers or {}
-    response = requests.get(url, headers=headers, params=params, timeout=5)
+    response = requests.get(url, headers=headers, params=params, timeout=10)
     response.raise_for_status()
-    pprint(response.json())
     return response.json()
 
 
@@ -54,9 +52,8 @@ if __name__ == '__main__':
     env = Env()
     env.read_env()
     sj_secret_key = env.str('SUPERJOB_KEY')
-    hh_town = 1
     sj_town = 'Москва'
-    num_vacancies = 100
+    hh_town = 1
 
     hh_url = 'https://api.hh.ru/vacancies'
     sj_url = 'https://api.superjob.ru/2.0/vacancies/'
@@ -65,42 +62,62 @@ if __name__ == '__main__':
     }
 
     hh_params = {
-        'per_page': num_vacancies,
+        'per_page': 10,
         'text': None,
         'area': hh_town,
+        'page': 0,
     }
     sj_params = {
         'keywords': None,
         'town': sj_town,
-        'count': num_vacancies,
+        'page': 0,
+        'count': 40,
     }
 
     programming_languages = [
         'JavaScript',
-        # 'Java',
-        # 'Python',
-        # '1С программист',
-        # 'PHP',
-        # 'C++',
-        # 'C#',
-        # 'C программист',
+        'Java',
+        'Python',
+        '1С программист',
+        'PHP',
+        'C++',
+        'C#',
+        'C программист',
     ]
     hh_vacancies_statistics = [['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']]
     sj_vacancies_statistics = [['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']]
 
     for language in programming_languages:
         hh_params['text'] = language
+        hh_all_vacansies = []
+        hh_pages = 1
         sj_params['keywords'] = language
-        hh_vacancies = request_response(hh_url, hh_params)
-        sj_vacansies = request_response(sj_url, sj_params, sj_headers)
+        sj_all_vacansies = []
 
-        hh_num_vacancies = predict_salary(hh_vacancies['items'], predict_rub_salary_hh)[0]
-        hh_average_salary = predict_salary(hh_vacancies['items'], predict_rub_salary_hh)[1]
-        sj_num_vacancies = predict_salary(sj_vacansies['objects'], predict_rub_salary_sj)[0]
-        sj_average_salary = predict_salary(sj_vacansies['objects'], predict_rub_salary_sj)[1]
+        while hh_params['page'] < hh_pages:
+            hh_vacancies = request_response(hh_url, hh_params)
+            hh_all_vacansies.extend(hh_vacancies['items'])
+            hh_params['page'] += 1
+            hh_pages = hh_vacancies['pages']
+
+        while True:
+            sj_vacansies = request_response(sj_url, sj_params, sj_headers)
+            sj_all_vacansies.extend(sj_vacansies['objects'])
+            sj_params['page'] += 1
+
+            if not sj_vacansies['more']:
+                break
+
+            sj_total_vacancies = sj_vacansies['total']
+
+
+        hh_num_vacancies = predict_salary(hh_all_vacansies, predict_rub_salary_hh)[0]
+        hh_average_salary = predict_salary(hh_all_vacansies, predict_rub_salary_hh)[1]
+        sj_num_vacancies = predict_salary(sj_all_vacansies, predict_rub_salary_sj)[0]
+        sj_average_salary = predict_salary(sj_all_vacansies, predict_rub_salary_sj)[1]
 
         hh_vacancies_statistics.append([language, hh_vacancies['found'], hh_num_vacancies, hh_average_salary])
-        sj_vacancies_statistics.append([language, sj_vacansies['total'], sj_num_vacancies, sj_average_salary])
+        sj_vacancies_statistics.append([language, sj_total_vacancies, sj_num_vacancies, sj_average_salary])
 
     hh_table = AsciiTable(hh_vacancies_statistics)
     sj_table = AsciiTable(sj_vacancies_statistics)
