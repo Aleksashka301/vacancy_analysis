8919b1de2b5a3edb40731a6e75b29f1e43d8d34a
Aleksashka301 <aleksashka301@gmail.com>
2025-02-25 19:24:03 +0500

    code drying )

--- vacancy_statistics_module.py
+++ vacancy_statistics_module.py
@@ -3,7 +3,7 @@ def predict_rub_salary_hh(vacancy):
     if not income or income['currency'] != 'RUR':
         return None
 
-    return calculation_income(income, 'from', 'to')
+    return calculate_income(income, 'from', 'to')
 
 
 def predict_rub_salary_sj(vacancy):
@@ -12,10 +12,10 @@ def predict_rub_salary_sj(vacancy):
     elif vacancy['currency'] != 'rub':
         return None
 
-    return calculation_income(vacancy, 'payment_from', 'payment_to')
+    return calculate_income(vacancy, 'payment_from', 'payment_to')
 
 
-def calculation_income(vacancy, income_from, income_to):
+def calculate_income(vacancy, income_from, income_to):
     if vacancy[income_from] and vacancy[income_to]:
         return (vacancy[income_from] + vacancy[income_to]) / 2
     elif not vacancy[income_to]:
@@ -25,21 +25,13 @@ def calculation_income(vacancy, income_from, income_to):
 
 
 def predict_salary(vacancies, predict_rub_salary):
-    vacancy_analytics = {}
     sum_income, count = 0, 0
-
     for vacancy in vacancies:
         expected_income = predict_rub_salary(vacancy)
         if expected_income:
             sum_income += expected_income
             count += 1
-
-    vacancy_analytics['num_vacancies'] = count
     try:
-        vacancy_analytics['average_income'] = int(sum_income / count)
+        return count, int(sum_income / count)
     except ZeroDivisionError:
-        vacancy_analytics['num_vacancies'] = 0
-        vacancy_analytics['average_income'] = 0
-        return vacancy_analytics
-
-    return vacancy_analytics
+        return 0, 0
